c03cacc8ef3734dc0d6a41b8db134c0511fc19fe
rodrigo1857 <rodrigo.vega@unmsm.edu.pe>
2025-01-29 22:45:01 -0500

    add PostgreSQL configuration and environment support in NestJS application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
 import { ItemsModule } from './items/items.module';
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
     @Module({
       imports: [
+      ConfigModule.forRoot(),
       GraphQLModule.forRoot({
       driver: ApolloDriver,
       // debug: false,
@@ -17,6 +20,16 @@ import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
         ]
       }),
       ItemsModule,
+      TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST,
+        port: +process.env.DB_PORT,
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
   ],
   controllers: [],
   providers: [],
